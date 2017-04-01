dbec2aedc794921acc5b8357265edd48ac0681e2
loloputters <laura.pitt@live.ca>
2017-03-31 23:37:10 -0400

    del
    del

--- loretta/js/main.js
+++ loretta/js/main.js
@@ -1,13 +1,58 @@
-$(function() {
-    $('#slider1').slider({
-        value:14,
-        min: 8,
-        max: 42,
-        step: 1,
-        slide: function( event, ui ) {
-            $('#font_size').val(ui.value + ' px');
-            $('.box').css('font-size', ui.value);
-        }
+$(document).ready(function(){
+
+    // Preventing Scroll Reset
+	$('a[href^="#"]').on('click',function (e) {
+	    e.preventDefault();
+
+        /* this part wasn't been used
+
+	    var target = this.hash;
+	    var $target = $(target);
+	    $('html, body').stop().animate({
+	        'scrollTop': $target.offset().top
+	    }, 1000, 'swing', function () {
+	        window.location.hash = target;
+	    });
+
+        */
+	});
+
+
+
+    // Handles the navigation
+    $(".characters, .fontsize, .details").hide();
+
+    $("nav ul li:nth-child(1)").click(function(){
+    		$(".about").fadeToggle(1500);
+    		$(".characters, .fontsize, .details").hide();
     });
-    $('#font_size').val($('#slider1').slider('value') + ' px');
+    $("nav ul li:nth-child(2)").click(function(){
+    		$(".characters").fadeToggle(1500);
+    		$(".about, .fontsize, .details").hide();
+    });
+    $("nav ul li:nth-child(3)").click(function(){
+    		$(".fontsize").fadeToggle(1500);
+    		$(".about, .characters, .details").hide();
+    });
+    $("nav ul li:nth-child(4)").click(function(){
+    		$(".details").fadeToggle(1500);
+    		$(".about, .characters, .fontsize").hide();
+    });
+
+
+
+    // Slider Events: called whenever it changes
+    $(".slider").on("input", function(){
+        // Assigning it to a variable
+        var size = this.value;
+
+        // Setting the font size label
+        $('#font_size').val(size + ' px');
+        // Setting the actual size of the font
+        $('.box').css({ 'font-size': parseInt(size) });
+    });
+
+
+
+
 });
