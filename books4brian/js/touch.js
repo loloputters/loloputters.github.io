1e43d0a54f9cac5b6ef74775421a374eb0e7ecdb
loloputters <laura.pitt@live.ca>
2017-04-17 13:28:07 -0400

    deleted
    fuck

--- books4brian/js/touch.js
+++
@@ -1,9 +0,0 @@
-jQuery(function($) {
-    $('#img').mouseover(function() {
-        var dWidth = $(document).width() - 50, // 100 = image width
-            dHeight = $(document).height() - 50, // 100 = image height
-            nextX = Math.floor(Math.random() * dWidth),
-            nextY = Math.floor(Math.random() * dHeight);
-        $(this).animate({ left: nextX + 'px', top: nextY + 'px'});
-    });
-});
